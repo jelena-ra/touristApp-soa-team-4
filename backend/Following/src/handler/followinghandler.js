1e807b9cfea9755f1b9c51ea739ff6d6e31b5195
bteodora <149783651+bteodora@users.noreply.github.com>
2025-08-18 17:37:16 +0200

    Minor changes

--- backend/Following/src/handler/followinghandler.js
+++ backend/Following/src/handler/followinghandler.js
@@ -21,17 +21,12 @@ const followingHandler = {
   getRecommendations: async (call, callback) => {
     try {
       const { userId } = call.request;
-      console.log(`Handler je primio zahtev za preporuke za korisnika: ${userId}`);
 
-      // 2. Čekamo (await) da servis završi svoj posao i vrati nam listu profila
       const profiles = await followingService.getRecommendations(userId);
 
-      // 3. Ako je sve prošlo bez greške, pozivamo callback da pošaljemo uspešan odgovor
-      //    Odgovor je objekat sa ključem 'profiles', kako je definisano u .proto fajlu
       callback(null, { profiles: profiles });
 
     } catch (error) {
-      // Ako se desi greška u 'await' liniji iznad, kod skače direktno ovde
       console.error("Greška u handleru prilikom dohvatanja preporuka:", error.message);
       callback({
         code: 5,
